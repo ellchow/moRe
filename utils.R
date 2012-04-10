6892adf83248a9aa81c6b86bf20ec0f3ab30ab0a
Elliot Chow <drag0nxspirit@gmail.com>
2012-04-09 22:24:52 -0700

    add weighted bootstrap

--- utils.R
+++ utils.R
@@ -156,6 +156,24 @@ linear.norm <- function(x, lb, ub, clipMin=FALSE, clipMax=FALSE, rm.na=TRUE, dis
   y
 }
 
+mean.cl.boot.w <- function(x,w=rep(1,length(x)),rounds=1000,ci=0.95,na.rm=T){
+  if(na.rm){
+    ok <- !is.na(x) & !is.nan(x) & !is.infinite(x)
+    w <- w[ok]
+    x <- x[ok]
+  }
+  w <- w / sum(w)
+  z <- c(x %*% w,
+         quantile(sapply(1:rounds,
+                         function(i,x,m){
+                           sum(sample(x,m,replace=T,prob=w))/m
+                         },
+                           x=x,m=length(x)),
+                         c(1-ci,1+ci)/2))
+  names(z) <- c('y','ymin','ymax')
+  as.data.frame(as.list(z))
+}
+
 lzip <- function(...){
   args <- list(...)
   n <- min(sapply(args,length))
