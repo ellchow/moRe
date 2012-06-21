4535ddf4d9552e77bde151dfb5aae42b987052ce
ellchow <drag0nxspirit@gmail.com>
2012-06-21 11:01:17 -0700

    Update master

--- mdls.R
+++ mdls.R
@@ -350,10 +350,13 @@ gbm.plot <- function (x, i.var = 1, n.trees = x$n.trees, continuous.resolution =
   if (length(i.var) == 1) {
     if (!f.factor) {
       j <- order(X$X1)
-      ggplot(X, aes(x=X1,y=y)) + geom_step(color='steelblue',direction='hv') + geom_point(color='red',alpha=0.65) + scale_x_continuous(x$var.names[i.var]) + scale_y_continuous(paste("f(", x$var.names[i.var], ")", sep = ""))
+      plot(X$X1, X$y, type = "l", xlab = x$var.names[i.var],
+           ylab = paste("f(", x$var.names[i.var], ")", sep = ""),
+           ...)
     }
     else {
-      ggplot(X, aes(x=X1,y=y)) + geom_boxplot(color='steelblue') + scale_x_discrete(x$var.names[i.var]) + scale_y_continuous(paste("f(", x$var.names[i.var], ")", sep = ""))
+      plot(X$X1, X$y, xlab = x$var.names[i.var], ylab = paste("f(",
+                                                   x$var.names[i.var], ")", sep = ""), ...)
     }
   }
   else if (length(i.var) == 2) {
@@ -406,6 +409,4 @@ gbm.plot <- function (x, i.var = 1, n.trees = x$n.trees, continuous.resolution =
     }
   }
 }
-
-
 
