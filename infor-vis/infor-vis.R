dac97535740e87325eae15d09577db394a3d49b7
ellchow <drag0nxspirit@gmail.com>
2013-08-08 14:37:48 -0700

    checkin info-vis.R

---
+++ infor-vis/infor-vis.R
@@ -0,0 +1,87 @@
+#!/usr/bin/env Rscript
+
+## Copyright 2013 Elliot Chow
+
+## Licensed under the Apache License, Version 2.0 (the "License")
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+
+## http://www.apache.org/licenses/LICENSE-2.0
+
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+import('shiny',
+       'utils',
+       'cmdargs',
+       'yaml')
+
+#### parse command line args
+raw.args <- commandArgs(T)
+args <- parse.args('infor-vis.R',
+                   list(
+                        list(name='-config',
+                             desc='path to config file',
+                             default='config.R'),
+                        list(name='-loglevel',
+                             desc='log level',
+                             default = SimpleLog.ERROR,
+                             parser = function(s){
+                               if(s == 'info') SimpleLog.INFO
+                               else if (s == 'error') SimpleLog.ERROR
+                               else if (s == 'debug') SimpleLog.DEBUG
+                               else NA
+                             })
+                        ),
+                   raw.args)
+
+logger <- SimpleLog('info-vis', args[['-loglevel']])
+timer <- Timer(logger)
+
+#### load config
+.config <- list()
+source(args[['-config']], chdir=T)
+
+## check config blocks present
+required.config.blocks <- c('models', 'data', 'query', 'query.id', 'display')
+missing.config.blocks <- required.config.blocks[!required.config.blocks %in% names(.config)]
+stop.if.not(length(missing.config.blocks) == 0, sprintf('missing configuration blocks: %s', paste(missing.config.blocks, collapse=',')))
+
+## check required columns, compute new columns, subset
+.config$required.columns <- c(.config$query, .config$query.id)
+missing.required.columns <- setdiff(.config$required.columns, names(.config$data))
+stop.if.not(length(missing.required.columns) == 0, 'missing required columns %s', paste(missing.required.columns, collapse=','))
+
+.config$for.display.columns <- c(unlist(lapply(.config$display, function(d) d[[1]])))
+missing.for.display.columns <- setdiff(.config$for.display.columns, names(.config$data))
+write.msg(logger, 'missing columns required for display %s', paste(missing.for.display.columns, collapse=','), level = 'warning')
+
+write.msg(logger, 'missing columns required for display %s', paste(missing.for.display.columns, collapse=','), level = 'warning')
+
+## load models and check features
+.config$models <- flatten(lapply(.config$models, load.data))
+.config$models <- Filter(function(m){
+  missing.features <- setdiff(m$features, names(.config$data))
+  can.evaluate <- length(missing.features) == 0
+
+  if(!can.evaluate)
+    write.msg(logger, 'model %s cannot be evaluated (missing features %s)', m$id, paste(missing.features, collapse=','), level='warning')
+
+  can.evaluate
+}, .config$models)
+
+stop.if(length(.config$models) == 0 && !(.config$truth %in% names(.config$data)), 'no models or true order to display')
+
+if(.config$drop){
+  keep <- unique(c(.config$required.columns, unlist(lapply(.config$models, function(m) m$features))))
+  write.msg(logger, 'only keeping columns %s', paste(keep, collapse=','))
+  .config$data <- subset(.config$data, select = keep)
+}
+
+
+#### start server
+runApp('./')
+
