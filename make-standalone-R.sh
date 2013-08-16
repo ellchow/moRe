43babc772d5e8ca37018c65ca8119ec0486228c9
ellchow <drag0nxspirit@gmail.com>
2013-08-16 14:13:02 -0700

    some upates

--- make-standalone-R.sh
+++ make-standalone-R.sh
@@ -23,11 +23,13 @@ if test -f "R-$R_VERSION/_BUILD_SUCCESS" ; then
     rm -f R-$R_VERSION/_PACKAGE_SUCCESS
     echo 'modifying R_HOME_DIR and copying libraries...'
     sed 0,/'R_HOME_DIR=.*'/{s/R_HOME_DIR=.*/'R_HOME_DIR=$(dirname $(readlink -f $0))\/..'/} R-$R_VERSION/bin/R > R.tmp && \
-        mv -v R-$R_VERSION/bin/R R-$R_VERSION/bin/R.bak && \
+        (test -f R-$R_VERSION/bin/R.bak && echo 'R.bak already exists' || mv -v R-$R_VERSION/bin/R R-$R_VERSION/bin/R.bak) && \
         mv -v R.tmp R-$R_VERSION/bin/R && \
-        ls `ldd R-$R_VERSION/bin/exec/R | awk -F'=>' '{print $2}' | awk -F'(' '!($0 ~ /not found/){print $1}' | awk -F'(' '/^[ ]*$/ {next} {print $0}'` | xargs -I {} cp -v {} R-\
-$R_VERSION/lib/.`)` && \
-        tar -czf R-$R_VERSION-build.tar.gz R-$R_VERSION && \
+        chmod a+rx R-$R_VERSION/bin/R && \
+        ls `ldd R-$R_VERSION/bin/exec/R | awk -F'=>' '{print $2}' | awk -F'(' '!($0 ~ /not found/){print $1}' | awk -F'(' '/^[ ]*$/ {next} {print $0}'` | xargs -I {} cp -v {} R-$R_VERSION/lib/. && \
+        cd R-$R_VERSION && \
+        tar -czf ../R-$R_VERSION-build.tar.gz . && \
+        cd .. && \
         touch R-$R_VERSION/_PACKAGE_SUCCESS
 else
     echo "ERROR: R-$R_VERSION was not built successfully"
