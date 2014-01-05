0b02f26c3e928284019b7f708ebadd904382bd2d
ellchow <zzzxqq@gmail.com>
2014-01-05 13:22:28 -0800

    mpt helpers

--- finance.R
+++ finance.R
@@ -17,13 +17,54 @@ import('utils', 'yahoofin', 'quadprog', 'reshape2')
 
 x <- load.data('~/Documents/investments/data/historical-data-standard.rda')
 
-stats <- yfin.stats(x)
+mpt.minvar.portfolio <- function(r, sigma, target.return, labels=names(r), return.qp = FALSE) {
+  n <- length(r)
+  A <- cbind(matrix(rep(1, n), nr=n), ## weights sum to 1
+             matrix(r, nr=n), ## achieves target return
+             diag(n) ## lower bounds of weights - currently, no shorting allowed
+             )
+  b <- c(1,
+         target.return,
+         rep(0,n)
+         )
 
-A <- cbind(
-  matrix(rep(1,stats$n), nr=stats$n),
-  diag(stats$n)
-)
-b <- c(1, rep(0,stats$n))
-r <- solve.QP(cov2cor(stats$cov), 100 * stats$mean, A, b, meq=1)
-w <- round(r$solution,4) %named% stats$symbols
-w %*% stats$mean
+  y <- solve.QP(sigma, rep(0, n), A, b, meq=2)
+
+  w <- round(y$solution,4) %named% labels
+  result <- list(w = w,
+                 mean = w %*% r,
+                 sd= sqrt(t(w) %*% sigma %*% w)
+                 )
+  if(return.qp)
+    result$qp <- y
+
+  result
+}
+
+mpt.efficient.frontier <- function(r, sigma, labels=names(r)) {
+  ws <- lapply(seq(max(min(stats$mean), 0), max(stats$mean), length=10), function(t) {
+    tryCatch(mpt.minvar.portfolio(stats$mean, stats$cov, t, stats$symbols, return.qp = F), error=function(e) NA)
+  })
+
+  as.data.frame(csplat(rbind, lapply(na.rm(ws), function(x) c(x$w, mean=x$mean, sd = x$sd))))
+}
+
+
+
+duration <- 260
+m <- 1
+stats <- yfin.stats(x, start.date=max(x$date) - duration * (m + 1), end.date=max(x$date) - duration * m)
+d <- mpt.efficient.frontier(stats$mean, stats$sigma)
+ggplot(d, aes(260 * mean, sd)) + geom_point() + geom_line() + coord_flip()
+
+
+####
+## A <- cbind(
+##            matrix(rep(1,stats$n), nr=stats$n),
+##            diag(stats$n)
+##            )
+## b <- c(1, rep(0,stats$n))
+## r <- solve.QP(cov2cor(stats$cov), 100 * stats$mean, A, b, meq=1)
+## w <- round(r$solution,4) %named% stats$symbols
+## w %*% stats$mean
+####
